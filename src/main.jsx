232ca094ef29f443919dfdf83ab6ec604253c62e
Evan Gipson <evangipson@gmail.com>
2025-03-16 16:23:16 -0500

    Fix app.jsx reference in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './app';
 import '../assets/css/reset.css';
 import '../assets/css/properties.css';
 import '../assets/css/layout.css';
